a09b01e22c0eb797d43b6b13aa20573ead0a071d
maimoona naz <maimoonanaz57@gmail.com>
2025-02-17 12:15:19 +0500

    push code to git

---
+++ number.py
@@ -0,0 +1,15 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print("sorry, guess again, Too low.")
+        elif guess > random_number:
+            print("sorry,guess again, Too high.")
+    print(f'yay,congrats, You have guessed the number🥳 {random_number} correctly!')
+
+guess(30)
+
